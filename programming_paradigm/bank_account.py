bc0fc6e875f11543cf008778aa6d094241d52413
Ol4k7 <tolulopetheophilus96@gmail.com>
2025-08-03 21:04:22 +0100

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,23 +1,18 @@
+# bank_account.py
+
 class BankAccount:
-    def __init__(self, account_holder, initial_balance=0):
-        self.account_holder = account_holder
-        self.balance = initial_balance
+    def __init__(self, initial_balance=0):
+        self.__account_balance = initial_balance  # Encapsulated attribute
 
     def deposit(self, amount):
         if amount > 0:
-            self.balance += amount
-            return f"Deposited: ${amount}"
-        else:
-            return "Deposit amount must be positive."
+            self.__account_balance += amount
 
     def withdraw(self, amount):
-        if amount > self.balance:
-            return "Insufficient funds."
-        elif amount <= 0:
-            return "Withdrawal amount must be positive."
-        else:
-            self.balance -= amount
-            return f"Withdrew: ${amount}"
+        if amount <= self.__account_balance:
+            self.__account_balance -= amount
+            return True
+        return False
 
     def display_balance(self):
-        return f"Current Balance: ${self.balance:.2f}"
+        print(f"Current Balance: ${self.__account_balance}")
